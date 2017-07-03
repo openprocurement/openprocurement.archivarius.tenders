ae55074e668f98913e218d144983c498b90579a1
Roman Kozlovskyi <kroman0@quintagroup.com>
2017-07-03 15:32:28 +0300

    Update sandbox filter rules

--- openprocurement/archivarius/tenders/resource.py
+++ openprocurement/archivarius/tenders/resource.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 from iso8601 import parse_date
 from datetime import timedelta
 
 STATUS = ['complete', 'unsuccessful', 'cancelled']
-TIMEDELTA = timedelta(days=100)
+SANDBOX_MODE = os.environ.get('SANDBOX_MODE', False)
+TIMEDELTA = timedelta(days=20) if SANDBOX_MODE else timedelta(days=100)
 
 
 def tender_filter(item, time):
